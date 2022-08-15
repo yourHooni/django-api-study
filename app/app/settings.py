64e3a7537e0aaef0e0970d0fea4799dd85465e03
jhoon6886@gmail.com <pwhyun6246>
2022-08-15 23:14:26 +0900

    fix: database setting

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': os.environ.get('DB_HOST'),
+        'PORT': 5432,
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
