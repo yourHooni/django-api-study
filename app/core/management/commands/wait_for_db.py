6d797d8184ed01eaa9e3a4830858c5b2405020cd
jhoon6886@gmail.com <pwhyun6246>
2022-08-15 23:32:01 +0900

    fix: wait_for_db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -17,7 +17,7 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                self.check(databases=['default'])
+                self.check(database=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
