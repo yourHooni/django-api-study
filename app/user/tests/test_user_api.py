7184401734565b8e72e9dcbe98b90bf31c388f9c
jhoon6886@gmail.com <pwhyun6246>
2022-08-22 01:17:08 +0900

    test: user 관리 작성

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -2,18 +2,18 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, force_authenticate
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
-# TOKEN_URL = reverse('user:token')
+MANAGE_USER_URL = reverse('user:manage')
 
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
 
-class UserApiTests(TestCase):
+class PublicUserApiTests(TestCase):
     """Test the users API (public)"""
 
     def setUp(self):
@@ -54,3 +54,48 @@ class UserApiTests(TestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication is required for users."""
+        res = self.client.get(MANAGE_USER_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test API requires that require authentication."""
+
+    def setUp(self):
+        self.user = create_user(
+            email='test@test.com',
+            password='test1234',
+            name='test'
+        )
+        self.client = APIClient()
+        # force_authenticate: 인증이 필요한 요청에 대해 강제로 자격부여
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in used."""
+        res = self.client.get(MANAGE_USER_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_manage_not_allowd(self):
+        """Test that POST is not allowed on the my url."""
+        res = self.client.post(MANAGE_USER_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user."""
+        payload = {"name": "new test", "password": "newtest1234"}
+        res = self.client.patch(MANAGE_USER_URL, payload)
+
+        # refresh_from_db: 모델 객체정보를 다시 불러옴
+        self.user.refresh_from_db()
+
+        self.assertEqual(self.user.name, payload["name"])
+        self.assertTrue(self.user.check_password(payload["password"]))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
