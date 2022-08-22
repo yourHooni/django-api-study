24108a3e47614bfaf43eb72bf1ba57b23a4f8162
jhoon6886@gmail.com <pwhyun6246>
2022-08-22 10:15:43 +0900

    docs: user model 참조 방법

--- app/user/models.py
+++ app/user/models.py
@@ -8,6 +8,17 @@ from django.contrib.auth.models import (
     PermissionsMixin
 )
 
+# User model 참조 방법
+# 1. from django.contrib.auth.models import User
+#    -> 유지보수 어려움
+# 2. from django.contrib.auth import get_user_model
+#    -> 객체 인스턴스 리턴
+#       django 앱이 로드되는 순간 실행, 유효하지 않은 사용자 모델 객체가 리턴될수 있음
+#       INSTALLED_APPS 변경 등 앱 다시 로드시 문제 생길 확률 높아짐
+# 3. (추천) from django.conf import settings: settings.AUTH_USER_MODEL
+#    -> 외래키 모델 전달시 문자열 리턴
+#       외래키가 임포트될 때 모델 클래스 탐색 실패시, 모든 앱이 로드될때까지 탐색 중단
+#       항상 올바른 사용자 모델을 받을수 있음
 
 # 인증 관련
 # django.contrib.auth: 장고에서 제공하는 인증시스템
