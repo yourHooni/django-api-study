9a842e92d2d29660b6de0259ca81941240078034
jhoon6886@gmail.com <pwhyun6246>
2022-08-23 20:18:58 +0900

    test: Recipe model test 작성

--- app/recipe/tests/test_models.py
+++ app/recipe/tests/test_models.py
@@ -34,3 +34,17 @@ class IngredientTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+
+class RecipeTests(TestCase):
+    """ Test the recipe model. """
+    def test_recipe_str(self):
+        """Test the recipe string representation."""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
